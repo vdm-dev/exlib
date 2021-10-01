58a44baccc5bfc76b8c33f11682eb8d63e1db2e6
vdm-dev <vdm.inbox@gmail.com>
2021-10-01 16:26:12 +0400

    Test module for ex_strlcat()

--- tests/test-strlcat.c
+++ tests/test-strlcat.c
@@ -54,7 +54,6 @@ void signal_handler (int signo)
 int main(void)
 {
     char* buffer = NULL;
-    char* buffer2 = NULL;
     char* position = NULL;
     char* buffer_end = NULL;
     size_t length = 0;
@@ -62,9 +61,8 @@ int main(void)
     volatile int failures = 0;
 
     buffer = malloc(buffer_size);
-    buffer2 = malloc(buffer_size);
 
-    if (buffer == NULL || buffer2 == NULL)
+    if (buffer == NULL)
     {
         fprintf(stderr, "unable to allocate memory\n");
         return 1;
@@ -73,86 +71,91 @@ int main(void)
     memset(buffer, 'z', buffer_size);
     buffer_end = buffer + buffer_size;
 
-    /* Test copying to a zero-length NULL buffer. */
-    length = ex_strlcpy(NULL, "abcd", 0);
-    if (length != 4)
+    /* Test appending to an unterminated string. */
+    length = ex_strlcat(buffer, "abcd", buffer_size);
+    if (length != 4 + buffer_size)
     {
-        fprintf(stderr, "ex_strlcpy: failed zero-length buffer test (1a)\n");
+        fprintf(stderr,
+                "ex_strlcat: failed unterminated buffer test (1a)\n");
         failures++;
     }
-
-    /* Test copying small string to a large buffer. */
-    length = ex_strlcpy(buffer, "abcd", buffer_size);
-    if (length != 4)
+    /* Make sure we only wrote where expected. */
+    for (position = buffer; position < buffer_end; position++)
     {
-        fprintf(stderr, "ex_strlcpy: failed large buffer test (2a)\n");
-        failures++;
+        if (*position != 'z')
+        {
+            fprintf(stderr,
+                    "ex_strlcat: failed unterminated buffer test (1b)\n");
+            failures++;
+            break;
+        }
     }
 
+    /* Test appending to a full string. */
+    buffer_end[-1] = '\0';
+    length = ex_strlcat(buffer, "abcd", buffer_size);
+    if (length != 4 + buffer_size - 1)
+    {
+        fprintf(stderr, "ex_strlcat: failed full buffer test (2a)\n");
+        failures++;
+    }
+    /* Make sure we only wrote where expected. */
+    for (position = buffer; position < buffer_end - 1; position++)
+    {
+        if (*position != 'z')
+        {
+            fprintf(stderr,
+                    "ex_strlcat: failed full buffer test (2b)\n");
+            failures++;
+            break;
+        }
+    }
+
+    /* Test appending to an empty string. */
+    buffer_end[-1] = 'z';
+    buffer[0] = '\0';
+    length = ex_strlcat(buffer, "abcd", buffer_size);
+    if (length != 4)
+    {
+        fprintf(stderr, "ex_strlcat: failed empty buffer test (3a)\n");
+        failures++;
+    }
     /* Make sure we only wrote where expected. */
     if (memcmp(buffer, "abcd", sizeof("abcd")) != 0)
     {
-        fprintf(stderr, "ex_strlcpy: failed large buffer test (2b)\n");
+        fprintf(stderr, "ex_strlcat: failed empty buffer test (3b)\n");
         failures++;
     }
     for (position = buffer + length + 1; position < buffer_end; position++)
     {
         if (*position != 'z')
         {
-            fprintf(stderr, "ex_strlcpy: failed large buffer test (2c)\n");
+            fprintf(stderr,
+                    "ex_strlcat: failed empty buffer test (3c)\n");
             failures++;
             break;
         }
     }
 
-    /* Test copying large string to a small buffer. */
-    memset(buffer, 'z', buffer_size);
-    memset(buffer2, 'x', buffer_size - 1);
-    buffer2[buffer_size - 1] = '\0';
-    length = ex_strlcpy(buffer, buffer2, buffer_size / 2);
-    if (length != buffer_size - 1)
+    /* Test appending to a NUL-terminated string. */
+    memcpy(buffer, "abcd", sizeof("abcd"));
+    length = ex_strlcat(buffer, "efgh", buffer_size);
+    if (length != 8)
     {
-        fprintf(stderr, "ex_strlcpy: failed small buffer test (3a)\n");
+        fprintf(stderr, "ex_strlcat: failed empty buffer test (4a)\n");
         failures++;
     }
-
     /* Make sure we only wrote where expected. */
-    length = (buffer_size / 2) - 1;
-    if (memcmp(buffer, buffer2, length) != 0 || buffer[length] != '\0')
+    if (memcmp(buffer, "abcdefgh", sizeof("abcdefgh")) != 0)
     {
-        fprintf(stderr, "ex_strlcpy: failed small buffer test (3b)\n");
+        fprintf(stderr, "ex_strlcat: failed empty buffer test (4b)\n");
         failures++;
     }
     for (position = buffer + length + 1; position < buffer_end; position++)
     {
-        if (*position != 'z')
-        {
-            fprintf(stderr, "ex_strlcpy: failed small buffer test (3c)\n");
-            failures++;
-            break;
-        }
-    }
-
-    /* Test copying to a 1-byte buffer. */
-    memset(buffer, 'z', buffer_size);
-    length = ex_strlcpy(buffer, "abcd", 1);
-    if (length != 4)
-    {
-        fprintf(stderr, "ex_strlcpy: failed 1-byte buffer test (4a)\n");
-        failures++;
-    }
-
-    /* Make sure we only wrote where expected. */
-    if (buffer[0] != '\0')
-    {
-        fprintf(stderr, "ex_strlcpy: failed 1-byte buffer test (4b)\n");
-        failures++;
-    }
-    for (position = buffer + 1; position < buffer_end; position++)
-    {
-        if (*position != 'z')
-        {
-            fprintf(stderr, "ex_strlcpy: failed 1-byte buffer test (4c)\n");
+        if (*position != 'z') {
+            fprintf(stderr,
+                    "ex_strlcat: failed empty buffer test (4c)\n");
             failures++;
             break;
         }
@@ -161,7 +164,7 @@ int main(void)
     /*
      * The following tests should result in SIGSEGV, however some
      * systems may erroneously report SIGBUS.
-     * These tests assume that strlcpy() is signal-safe.
+     * These tests assume that ex_strlcat() is signal-safe.
      */
 
     /* Test copying to a NULL buffer with non-zero size. */
@@ -169,32 +172,33 @@ int main(void)
     signal(SIGSEGV, &signal_handler);
     if (setjmp(jump_buffer) == 0)
     {
-        length = ex_strlcpy(NULL, "abcd", sizeof(buffer));
-        fprintf(stderr, "ex_strlcpy: failed NULL dst test (5a), "
-            "expected signal %d, got len %zu\n", SIGSEGV, length);
+        length = ex_strlcat(NULL, "abcd", sizeof(buffer));
+        fprintf(stderr, "ex_strlcat: failed NULL dst test (5a), "
+                        "expected signal %d, got len %zu\n", SIGSEGV, length);
         failures++;
     }
     else if (got_signal != SIGSEGV)
     {
-        fprintf(stderr, "ex_strlcpy: failed NULL dst test (5b), "
-            "expected signal %d, got %d\n", SIGSEGV, got_signal);
+        fprintf(stderr, "ex_strlcat: failed NULL dst test (5b), "
+                        "expected signal %d, got %d\n", SIGSEGV, got_signal);
         failures++;
     }
 
     /* Test copying from a NULL src. */
+    memcpy(buffer, "abcd", sizeof("abcd"));
     got_signal = 0;
     signal(SIGSEGV, &signal_handler);
     if (setjmp(jump_buffer) == 0)
     {
-        length = ex_strlcpy(buffer, NULL, sizeof(buffer));
-        fprintf(stderr, "ex_strlcpy: failed NULL src test (6a), "
-            "expected signal %d, got len %zu\n", SIGSEGV, length);
+        length = ex_strlcat(buffer, NULL, sizeof(buffer));
+        fprintf(stderr, "ex_strlcat: failed NULL src test (6a), "
+                        "expected signal %d, got len %zu\n", SIGSEGV, length);
         failures++;
     }
     else if (got_signal != SIGSEGV)
     {
-        fprintf(stderr, "ex_strlcpy: failed NULL src test (6b), "
-            "expected signal %d, got %d\n", SIGSEGV, got_signal);
+        fprintf(stderr, "ex_strlcat: failed NULL src test (6b), "
+                        "expected signal %d, got %d\n", SIGSEGV, got_signal);
         failures++;
     }
 
